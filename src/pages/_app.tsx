28910267ffde6cbc80491b869e700df4937f5af8
Gabin Marignier <gabin.contact@gmail.com>
2020-10-04 14:49:21 +0200

    Reset basic CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from "next/app";
 import { MDXProvider } from "@mdx-js/react";
 
+// Reset CSS
+import "normalize.css";
+
+// Style basic elements globally
+import "@/styles/globals.css";
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MDXProvider components={{}}>
