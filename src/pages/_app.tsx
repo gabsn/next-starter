b4dca08de7780063e3bb26dc6996d353dc729f7c
Gabin Marignier <gabin.contact@gmail.com>
2020-10-04 13:35:58 +0200

    Add missing types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { MDXProvider } from "@mdx-js/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MDXProvider>
+    <MDXProvider components={{}}>
       <Component {...pageProps} />
     </MDXProvider>
   );
