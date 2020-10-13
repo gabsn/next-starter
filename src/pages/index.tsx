dfd525fd8729695cc1d9eb3c98d36a095cf0b008
Gabin Marignier <gabin.contact@gmail.com>
2020-10-13 16:40:27 +0200

    Show user agent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { H1 } from "@/components/H1";
 
-const Home: React.FC = () => (
-  <>
-    <H1>Home</H1>
-  </>
-);
+const Home: React.FC = () => {
+  const [userAgent, setUserAgent] = useState(null);
+  useEffect(() => {
+    setUserAgent(navigator.userAgent);
+  }, []);
+  return (
+    <>
+      <H1>Your User Agent</H1>
+      <p>{userAgent}</p>
+      <style jsx>
+        {`
+          p {
+            padding: 64px 32px;
+          }
+        `}
+      </style>
+    </>
+  );
+};
 
 export default Home;
